9fef7a126226c50f226955a729fb9170e05e81f7
Egbuchiri Chibuikem Josiah <egbuchirichibuikem@gmail.com>
2025-12-24 20:42:15 +0000

    Implement greedy coin change algorithm in Python

---
+++ problem_set-6/sentimental-cash/cash.py
@@ -0,0 +1,22 @@
+def main():
+    # Prompt user for change owed
+    while True:
+        try:
+            change = float(input("Change owed: "))
+            if change >= 0:
+                break
+        except ValueError:
+            pass
+
+    cents = round(change * 100)
+
+    coins = 0
+
+    for coin in [25, 10, 5, 1]:
+        coins += cents // coin
+        cents %= coin
+
+    print(coins)
+
+if __name__ == "__main__":
+    main()
